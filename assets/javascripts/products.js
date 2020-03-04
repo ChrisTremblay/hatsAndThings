fea839bd808d2c85e5c2d8abad733e9ebae2b415
Tremblay Christopher Claude Giuseppe <ccgtremb@intranet.epfl.ch>
2020-03-04 16:28:52 +0100

    Add the toggle of highlight filter

--- assets/javascripts/products.js
+++ assets/javascripts/products.js
@@ -1,4 +1,7 @@
+/**** START OF HAT LOGIC PART 1 ****/
+
 class Hat {
+    /* Constructor method of the class Hat */
     constructor(name, price, color, imageHref) {
         this.name = name;
         this.price = price;
@@ -6,15 +9,16 @@ class Hat {
         this.imageHref = imageHref;
     }
 
-    // Adding a method to the constructor
+    // Return the formated string of the hat
     toString() {
         return `name: ${this.name}, color: ${this.color}, price: ${this.price}, image: ${this.imageHref}`;
     }
+    //Create a new node div and add the right classes before adding the correct inner html depending on the object properties defined
     displayHat() {
         let hatHtml = document.createElement('div');
         hatHtml.classList.add("accessory", "col-sm-4");
         hatHtml.innerHTML =
-        `<div class="card my-3">
+            `<div class="card my-3">
             <div class="currency btn btn-light disabled">${this.price}</div>
             <img class="card-img-top" src="${this.imageHref}" alt="Image of ${this.name}">
             <div class="card-body text-center">
@@ -27,6 +31,17 @@ class Hat {
     }
 }
 
+//Function to display all the hats created in the array
+renderHats = (hats) => {
+    hats.forEach(e => {
+        productContainer.appendChild(e.displayHat());
+    });
+}
+
+//Get the container of all hats
+let productContainer = document.getElementById("products");
+
+//Array of all the hats present in the started HTML page
 allHats = [
     new Hat("Baseball cap", 11.99, "red", "./assets/images/red/hats/1.png"),
     new Hat("Baseball cap", 11.99, "blue", "./assets/images/blue/hats/1.png"),
@@ -43,8 +58,29 @@ allHats = [
     new Hat("Trilby", 10.99, "yellow", "./assets/images/yellow/hats/4.png")
 ];
 
-let productContainer = document.getElementById("products");
+//Call the render hat function to render the html on the page
+renderHats(allHats);
 
-allHats.forEach( e => {
-    productContainer.appendChild(e.displayHat());
+/**** END OF HAT LOGIC PART 1 ****/
+
+
+
+/**** START OF FILTER BY COLOR LOGIC PART 2 ****/
+highlightSelectedFilter = (selectedFilter) => {
+    let allFilters = selectedFilter.parentElement.children;
+    
+    [...allFilters].forEach(e => {
+        if(e.getAttribute("class").includes("active")){
+            e.classList.remove("active");
+        }
+    });
+    selectedFilter.classList.add("active");
+}
+
+let filtersParent = document.querySelector("#filters .btn-group");
+
+filtersParent.addEventListener("click", e => {
+    highlightSelectedFilter(e.target);
 });
+
+/**** END OF FILTER BY COLOR LOGIC PART 2 ****/
