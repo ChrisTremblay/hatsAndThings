5c3d7bb2bc7acbb06fd773dc5e1c3258ab3f6abe
Tremblay Christopher Claude Giuseppe <ccgtremb@intranet.epfl.ch>
2020-03-05 13:46:58 +0100

    Fix a bug where the wishlist would not update after first 3 pick items

--- assets/javascripts/wishlist.js
+++ assets/javascripts/wishlist.js
@@ -1,9 +1,9 @@
 //Retrieve wishlist from session storage
-let retrieveWishlist = () =>{
+let retrieveWishlist = () => {
     let wishList = [];
-    for(let i=1; i<=3; i++){
+    for (let i = 1; i <= 3; i++) {
         //Build again the list of object from the string stored in the session storage
-        if(JSON.parse(sessionStorage.getItem(`accessory${i}`))){
+        if (JSON.parse(sessionStorage.getItem(`accessory${i}`))) {
             wishList.push(JSON.parse(sessionStorage.getItem(`accessory${i}`)));
         }
     }
@@ -11,7 +11,7 @@ let retrieveWishlist = () =>{
 }
 
 //Create the HTML code corresponding to the object accessory
-let createHtmlAccessory = (accessory, index) =>{
+let createHtmlAccessory = (accessory, index) => {
     let accessoryHtml = document.createElement('div');
     accessoryHtml.classList.add("col-sm-4");
     //Here we use an attribute index to make it easier for us to follow the deleting afterwards
@@ -33,7 +33,7 @@ let createHtmlAccessory = (accessory, index) =>{
 //Calls for each accessory the creation of the HTML and appends it to the container
 let displayWishlist = (accessories) => {
     accessories.forEach((e, index) => {
-        productContainer.appendChild(createHtmlAccessory(e, index+1));
+        productContainer.appendChild(createHtmlAccessory(e, index + 1));
     });
 }
 
@@ -41,6 +41,27 @@ let displayWishlist = (accessories) => {
 let removeFromWishlist = (htmlComponent, key) => {
     sessionStorage.removeItem(`accessory${key}`);
     htmlComponent.parentNode.removeChild(htmlComponent);
+
+    /*This next part is to handle the change of the indexs when deleting, so that we can go back to the shopping list and add more wishlist items without clearing the session storage */
+    //Here we re-index the data-index attribute to make it go again from 1
+    let newIndexAttribute = 1;
+    [...productContainer.children].forEach(e=>{
+        e.setAttribute(`data-index`, newIndexAttribute);
+        newIndexAttribute++;
+    });
+
+    //Here we reorganize the sessionStorage key values accessories so that they start again to 1
+    let tempStorage = [];
+    for(let i=1; i<=sessionStorage.length; i++){
+        if(sessionStorage.getItem(`accessory${i}`)){
+            tempStorage.push(sessionStorage.getItem(`accessory${i}`));
+        }
+    }
+    sessionStorage.clear();
+    for(let i = 0; i<tempStorage.length; i++){
+        sessionStorage.setItem(`accessory${i+1}`, tempStorage[i]);
+    }
+    /* Without this part, a bug where you cannot add more items appear */
 }
 
 //We listen on the products container to counteract the static listening of the queryselectorALL
@@ -56,6 +77,6 @@ productContainer.addEventListener("click", e => {
 //We initialize the page
 let wishList = retrieveWishlist();
 //And only display a wishlist if its not empty
-if(wishList.length != 0){
+if (wishList.length != 0) {
     displayWishlist(wishList);
 }
